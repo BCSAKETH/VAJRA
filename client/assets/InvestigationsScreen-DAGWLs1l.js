ef942efe4af4c48c53ac6a2d4c345d6cb1a2f58a
TanukuSai <saitanuku81@gmail.com>
2026-09-22 19:46:54 +0530

    fix(ui): restore clean minimalist reasoning indicator and rebuild client

--- client/assets/InvestigationsScreen-DAGWLs1l.js
+++ client/assets/InvestigationsScreen-DAGWLs1l.js
@@ -1,4 +1,4 @@
-import{c as P,u as R,r as s,A as C,j as e,X as I,l as L,at as $,au as M}from"./index-C-E9gQ5O.js";/**
+import{c as P,u as R,r as s,A as C,j as e,X as I,l as L,at as $,au as M}from"./index-BalM4cn_.js";/**
  * @license lucide-react v0.546.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- client/assets/hourglass-DQJxBd_w.js
+++ client/assets/hourglass-DQJxBd_w.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-C-E9gQ5O.js";/**
+import{c as a}from"./index-BalM4cn_.js";/**
  * @license lucide-react v0.546.0 - ISC
  *
  * This source code is licensed under the ISC license.
